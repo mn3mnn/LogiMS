c327050d8380234f8f072f200a7f3e6759922b98
monem <abdoeladawy4@gmail.com>
2025-09-29 18:37:40 +0300

    add drivers api v1

--- logims/config/api_router_v1.py
+++ logims/config/api_router_v1.py
@@ -3,11 +3,14 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
 from logims.users.api.views import UserViewSet
+from logims.drivers.api.views import (
+    DriverViewSet,
+)
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
-
+router.register("drivers", DriverViewSet, basename="driver")
 
 app_name = "api"
 urlpatterns = router.urls
--- logims/config/urls.py
+++ logims/config/urls.py
@@ -34,7 +34,7 @@ if settings.DEBUG:
 # API URLS
 urlpatterns += [
     # API base url
-    path("api/", include("config.api_router")),
+    path("api/v1/", include("config.api_router_v1"), name="api-v1"),
     # DRF auth token
     path("api/auth-token/", obtain_auth_token, name="obtain_auth_token"),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
